e4bc2c3a852530d89162f7bf1e3e22fa1707b4bb
mortenmkristensen <morten.m.kristensen@gmail.com>
2020-03-10 15:41:35 +0100

    Added technologies

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,3 +56,11 @@ end
 end
 
 puts "portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+
+puts "Technologies created"
